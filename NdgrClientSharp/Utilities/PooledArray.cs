8409497918b3803e848f267a3b72566169c37262
TORISOUP <tori.birdstrike@gmail.com>
2025-03-31 13:46:10 +0900

    PooledArrayのDispose修正

--- NdgrClientSharp/Utilities/PooledArray.cs
+++ NdgrClientSharp/Utilities/PooledArray.cs
@@ -1,25 +1,39 @@
 using System;
 using System.Buffers;
+using System.Threading;
 
 namespace NdgrClientSharp.Utilities
 {
-    public readonly struct PooledArray<T> : IDisposable
+    public struct PooledArray<T> : IDisposable
     {
-        public T[] Array { get; }
+        public T[] Array => _array ?? throw new ObjectDisposedException(nameof(PooledArray<T>));
         private readonly ArrayPool<T> _pool;
+        private readonly int _length;
+
+        private T[]? _array;
+        private int _disposed;
+
+        public int Length => _length;
+        public Span<T> Span => Array.AsSpan(0, _length);
 
         public PooledArray(int length, ArrayPool<T>? pool = null)
         {
             _pool = pool ?? ArrayPool<T>.Shared;
-            Array = _pool.Rent(length);
+            _array = _pool.Rent(length);
+            _length = length;
+            _disposed = 0;
         }
 
         public void Dispose()
         {
-            _pool.Return(Array);
+            if (Interlocked.Exchange(ref _disposed, 1) == 0)
+            {
+                if (_array != null)
+                {
+                    _pool.Return(_array);
+                    _array = null;
+                }
+            }
         }
-
-        public Span<T> AsSpan() => Array.AsSpan();
-        public Span<T> AsSpan(int start, int length) => Array.AsSpan(start, length);
     }
 }
